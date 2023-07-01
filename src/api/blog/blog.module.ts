e2aa8471e624460fb2c3ba0f026644650ba9165e
horlakz <officialhorlakz@gmail.com>
2023-07-01 23:03:37 +0100

    feat(src/api/blog/): adds blog-tag controller and service to blog module

--- src/api/blog/blog.module.ts
+++ src/api/blog/blog.module.ts
@@ -1,20 +1,22 @@
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { BlogService } from './services/blog.service';
 import { BlogController } from './controller/blog.controller';
-import { BlogCategoryService } from './services/category.service';
 import { BlogCategoryController } from './controller/category.controller';
-import { BlogCategory } from './entities/category.entity';
-import { BlogTag } from './entities/tag.entity';
-import { BlogComment } from './entities/comment.entity';
+import { BlogTagController } from './controller/tag.controller';
 import { Blog } from './entities/blog.entity';
+import { BlogCategory } from './entities/category.entity';
+import { BlogComment } from './entities/comment.entity';
+import { BlogTag } from './entities/tag.entity';
+import { BlogService } from './services/blog.service';
+import { BlogCategoryService } from './services/category.service';
+import { BlogTagService } from './services/tag.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([BlogCategory, BlogTag, BlogComment, Blog]),
   ],
-  controllers: [BlogController, BlogCategoryController],
-  providers: [BlogService, BlogCategoryService],
+  controllers: [BlogController, BlogCategoryController, BlogTagController],
+  providers: [BlogService, BlogCategoryService, BlogTagService],
 })
 export class BlogModule {}
