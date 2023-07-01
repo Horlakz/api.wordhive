50111d6ca473c6d8b26a3d8fd3fa26db430fa3cc
horlakz <officialhorlakz@gmail.com>
2023-07-01 15:06:30 +0100

    fix(src/api/auth/): updates users module import in file

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 
-import { UsersModule } from '@/users/users.module';
+import { UsersModule } from '@/api/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
