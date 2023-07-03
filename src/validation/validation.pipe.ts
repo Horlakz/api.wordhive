d945e2229af9832b7272007b645a691942bc2cf9
horlakz <officialhorlakz@gmail.com>
2023-07-03 11:35:59 +0100

    feat(src/validation/): creates new validation class for validation class validator classes

---
+++ src/validation/validation.pipe.ts
@@ -0,0 +1,34 @@
+import {
+  ArgumentMetadata,
+  Injectable,
+  PipeTransform,
+  BadRequestException,
+} from '@nestjs/common';
+import { validate } from 'class-validator';
+import { plainToInstance } from 'class-transformer';
+
+@Injectable()
+export class ValidationPipe implements PipeTransform {
+  async transform(value: any, { metatype }: ArgumentMetadata) {
+    if (!metatype || !this.toValidate(metatype)) {
+      return value;
+    }
+    const object = plainToInstance(metatype, value);
+    const errors = await validate(object);
+    if (errors.length > 0) {
+      const obj = errors[0].constraints;
+
+      throw new BadRequestException(obj[this.firstObjKey(obj)]);
+    }
+    return value;
+  }
+
+  private toValidate(metatype: Function): boolean {
+    const types: Function[] = [String, Boolean, Number, Array, Object];
+    return !types.includes(metatype);
+  }
+
+  private firstObjKey(obj: any): string {
+    return Object.keys(obj)[0];
+  }
+}
