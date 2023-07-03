8f9c9f6c0cfa53581dccf3f6861641444e7a6170
horlakz <officialhorlakz@gmail.com>
2023-07-03 04:01:59 +0100

    fix(src/api/blog/services/): removes unused import, adds blog service and remove blog repository, updates findAllByBlog function

--- src/api/blog/services/comment.service.ts
+++ src/api/blog/services/comment.service.ts
@@ -1,42 +1,31 @@
-import {
-  BadRequestException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
-import { BlogComment } from '../entities/comment.entity';
-import { Blog } from '../entities/blog.entity';
 import { CreateCommentDto } from '../dto/create-comment.dto';
+import { BlogComment } from '../entities/comment.entity';
+import { BlogService } from './blog.service';
 
 @Injectable()
 export class BlogCommentService {
   constructor(
     @InjectRepository(BlogComment)
     private blogCommentRepository: Repository<BlogComment>,
-    @InjectRepository(Blog)
-    private blogRepository: Repository<Blog>,
+    private blogService: BlogService,
   ) {}
 
   async create(createcomment: CreateCommentDto, blog_id): Promise<BlogComment> {
-    const blog = await this.blogRepository.findOneBy({
-      uuid: blog_id,
-    });
-    if (!blog) throw new NotFoundException('Blog not found');
+    const blog = await this.blogService.findOne(blog_id);
 
     const comment = new BlogComment();
     comment.fullname = createcomment.fullname;
     comment.message = createcomment.message;
-    comment.blog_id = blog;
+    comment.blog = blog;
 
     return this.blogCommentRepository.save(comment);
   }
 
   async findAllByBlog(uuid: string): Promise<BlogComment[]> {
-    const blog = await this.blogRepository.findOneBy({ uuid });
-    if (!blog) throw new NotFoundException('Blog not found');
-
-    return this.blogCommentRepository.findBy({ blog_id: blog });
+    return this.blogCommentRepository.find({ where: { blog: { uuid } } });
   }
 }
