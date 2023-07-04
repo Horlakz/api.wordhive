7740d61d855f15ea23f0b5a02040fa9c8146e9b9
horlakz <officialhorlakz@gmail.com>
2023-07-04 13:59:45 +0100

    feat(src/): rearranges imports in file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,15 @@ import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD, APP_PIPE } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 
-import { RequestLoggingMiddleware } from '@/middleware/logger.middleware';
 import { AuthGuard } from '@/api/auth/auth.guard';
 import { AuthModule } from '@/api/auth/auth.module';
-import { UsersModule } from '@/api/users/users.module';
 import { BlogModule } from '@/api/blog/blog.module';
-import { FaqModule } from '@/api/faq/faq.module';
 import { ContactModule } from '@/api/contact/contact.module';
+import { FaqModule } from '@/api/faq/faq.module';
+import { ShowcaseModule } from '@/api/showcase/showcase.module';
+import { UsersModule } from '@/api/users/users.module';
+import { RequestLoggingMiddleware } from '@/middleware/logger.middleware';
 import { ValidationPipe } from '@/validation/validation.pipe';
-import { ShowcaseModule } from './showcase/showcase.module';
 
 @Module({
   imports: [
