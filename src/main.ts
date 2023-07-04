6b508ef8316a93243b1fc7c3b3cba0e2d3f8b511
horlakz <officialhorlakz@gmail.com>
2023-07-05 00:08:37 +0100

    feat(src/): adds config for swagger docs;

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('Wordhive API')
+    .setDescription('Api for Wordhive')
+    .setVersion('0.1')
+    .addTag('wordhive')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(8000);
 }
 bootstrap();
