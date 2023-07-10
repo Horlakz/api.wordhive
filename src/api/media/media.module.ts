df2f3d11e6a20295eec9fda8d20a09a49bf206d7
horlakz <officialhorlakz@gmail.com>
2023-07-10 21:36:39 +0100

    feat(src/api/media/): removes aws module import

--- src/api/media/media.module.ts
+++ src/api/media/media.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 
-import { AwsModule } from '@/config/aws/aws.module';
 import { MediaController } from './media.controller';
 import { MediaService } from './media.service';
 
 @Module({
-  imports: [AwsModule],
   controllers: [MediaController],
   providers: [MediaService],
   exports: [MediaService],
