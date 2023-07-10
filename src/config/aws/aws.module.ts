ee51ae70a8dfa79b7d45556fdbec95089beb9f24
horlakz <officialhorlakz@gmail.com>
2023-07-10 21:35:51 +0100

    feat(src/config/): removes config folder and moves content to shared folder

--- src/config/aws/aws.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-
-import { AwsS3Service } from './aws_s3.service';
-
-@Module({
-  providers: [AwsS3Service],
-  exports: [AwsS3Service],
-})
-export class AwsModule {}
