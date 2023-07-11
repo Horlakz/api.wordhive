51ba30c4566e2fc9fe85002a1740b0aebf048e40
horlakz <officialhorlakz@gmail.com>
2023-07-11 08:44:52 +0100

    refactor(src/api/auth/): adds variable as a class variable not in function

--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -16,8 +16,9 @@ export class AuthService {
     private userService: UsersService,
   ) {}
 
+  private payload: { email: string; sub: string };
+
   async login(email: string, pass: string): Promise<any> {
-    let payload: { email: string; sub: string };
     try {
       const user = await this.userService.findOne(email);
 
@@ -30,30 +31,28 @@ export class AuthService {
       if (!comparePassword)
         throw new UnauthorizedException('Passwords do not match');
 
-      payload = { email: user.email, sub: user.uuid };
+      this.payload = { email: user.email, sub: user.uuid };
     } catch (err) {
       throw new BadRequestException(err.message);
     }
 
     return {
-      access_token: await this.jwtService.signAsync(payload),
+      access_token: await this.jwtService.signAsync(this.payload),
     };
   }
 
   async register(fullname: string, email: string, pass: string): Promise<any> {
-    let payload: { email: string; sub: string };
-
     try {
       const exists = await this.userService.findOne(email);
       if (exists) throw new BadRequestException('User already exists');
 
       const user = await this.userService.create(fullname, email, pass);
-      payload = { email: user.email, sub: user.uuid };
+      this.payload = { email: user.email, sub: user.uuid };
     } catch (err) {
       throw new BadRequestException(err.message);
     }
     return {
-      access_token: await this.jwtService.signAsync(payload),
+      access_token: await this.jwtService.signAsync(this.payload),
     };
   }
 }
