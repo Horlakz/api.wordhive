f6a1a7b0377773860e0360db7760a5b21a8f59b3
horlakz <officialhorlakz@gmail.com>
2023-07-12 11:38:24 +0100

    feat(src/): adds version to api routes

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { VersioningType } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
   const config = new DocumentBuilder()
     .setTitle('Wordhive API')
     .setDescription('Api for Wordhive')
