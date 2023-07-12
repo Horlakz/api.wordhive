12cbe186bea97c5b1501d0ed3dc034e7bf713810
horlakz <officialhorlakz@gmail.com>
2023-07-12 22:46:27 +0100

    feat(src/api/user/): updates UserService class name and adds verification-code service to providers and exports

--- src/api/user/user.module.ts
+++ src/api/user/user.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { User } from './entities/user.entity';
 import { UserController } from './user.controller';
-import { UserService } from './user.service';
+import { UserService } from './services/user.service';
+import { VerificationCodeService } from './services/verification-code.service';
+import { VerificationCode } from './entities/verification-code.entity';
+
+const providers = [UserService, VerificationCodeService];
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [UserService],
-  exports: [UserService],
+  imports: [TypeOrmModule.forFeature([User, VerificationCode])],
+  providers,
+  exports: [...providers],
   controllers: [UserController],
 })
 export class UserModule {}
