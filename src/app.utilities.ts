917bc591d69f079beb87f0f6a09e2394d640cc74
horlakz <officialhorlakz@gmail.com>
2023-07-12 18:18:16 +0100

    feat(src/): adds functions to generate random numbers

--- src/app.utilities.ts
+++ src/app.utilities.ts
@@ -26,4 +26,14 @@ export class AppUtilities {
   ): Promise<boolean> {
     return await bcrypt.compare(text, hash);
   }
+
+  public static generateRandomNumber(length: number = 6): string {
+    const digits = '0123456789';
+    let otp = '';
+    for (let i = 1; i <= length; i++) {
+      const index = Math.floor(Math.random() * digits.length);
+      otp = otp + digits[index];
+    }
+    return otp;
+  }
 }
