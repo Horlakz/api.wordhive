1d0f7e5e987f3182789dcf63be8a3bf6810d8855
horlakz <officialhorlakz@gmail.com>
2023-07-31 00:26:29 +0100

    feat(src/shared/): creates paystack service for payment functions and add service to shared module providers;

---
+++ src/shared/services/paystack.service.ts
@@ -0,0 +1,51 @@
+import { HttpService } from '@nestjs/axios';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class PaystackService {
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly configService: ConfigService,
+  ) {
+    this.httpService.axiosRef.defaults.baseURL =
+      'https://api.paystack.co/transaction';
+    this.httpService.axiosRef.defaults.headers.common[
+      'Authorization'
+    ] = `Bearer ${this.configService.get('PAYSTACK_SECRET_KEY')}`;
+    this.httpService.axiosRef.defaults.headers.common['Content-Type'] =
+      'application/json';
+  }
+
+  async initialize(data: {
+    email: string;
+    amount: number;
+    reference: string;
+    callback_url: string;
+  }) {
+    const { amount } = data;
+
+    try {
+      const response = await this.httpService.axiosRef.post(`/initialize`, {
+        ...data,
+        amount: amount * 100,
+      });
+      console.log(response);
+
+      return response.data;
+    } catch (e) {
+      throw new BadRequestException(e.response.data.message || e.message);
+    }
+  }
+
+  async verify(reference: string) {
+    try {
+      const { data } = await this.httpService.axiosRef.get(
+        `/verify/${reference}`,
+      );
+      return data;
+    } catch (e) {
+      throw new BadRequestException(e.response.data.message || e.message);
+    }
+  }
+}
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { Global, Module, type Provider } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 
 import { AwsS3Service } from './services/aws-s3.service';
 import { EmailService } from './services/email.service';
+import { PaystackService } from './services/paystack.service';
 
-const providers: Provider[] = [AwsS3Service, EmailService];
+const providers: Provider[] = [AwsS3Service, EmailService, PaystackService];
 
 @Global()
-@Module({ providers, exports: [...providers] })
+@Module({ imports: [HttpModule], providers, exports: [...providers] })
 export class SharedModule {}
