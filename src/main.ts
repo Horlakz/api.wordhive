361ba97c3fa9fd101037c709581c51a61e863a3b
horlakz <officialhorlakz@gmail.com>
2023-08-25 21:47:04 +0100

    feat(src/): adds static folder router to main app

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { VersioningType } from '@nestjs/common';
+import * as express from 'express';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use('/static', express.static('public'));
   app.enableCors();
   app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
   const config = new DocumentBuilder()
