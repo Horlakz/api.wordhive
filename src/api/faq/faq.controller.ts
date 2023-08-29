1c2f3e29718df9eaaf12ae15780c3a9a3398194a
horlakz <officialhorlakz@gmail.com>
2023-08-29 19:48:26 +0100

    feat(src/(src/api/faq/): removes unused code and adds query to function

--- src/api/faq/faq.controller.ts
+++ src/api/faq/faq.controller.ts
@@ -6,21 +6,17 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 
 import { Public } from '@/common/decorators/auth.public.decorator';
-import { EmailService } from '@/shared/services/email.service';
 import { CreateFaqDto } from './dto/create-faq.dto';
 import { UpdateFaqDto } from './dto/update-faq.dto';
 import { FaqService } from './faq.service';
-import { MailOptions } from '@/common/interfaces/mail-options';
 
 @Controller('faq')
 export class FaqController {
-  constructor(
-    private readonly faqService: FaqService,
-    private readonly emailService: EmailService,
-  ) {}
+  constructor(private readonly faqService: FaqService) {}
 
   @Post()
   create(@Body() createFaqDto: CreateFaqDto) {
@@ -29,8 +25,8 @@ export class FaqController {
 
   @Public()
   @Get()
-  async findAll() {
-    return await this.faqService.findAll();
+  async findAll(@Query('search') search: string) {
+    return await this.faqService.findAll(search);
   }
 
   @Patch(':id')
