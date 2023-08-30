db5c9e9f46dd0730765fcac25d5b1cf3629b1ef7
horlakz <officialhorlakz@gmail.com>
2023-08-30 20:38:59 +0100

    feat(src/api/contact/): adds email service to send message to contact

--- src/api/contact/contact.service.ts
+++ src/api/contact/contact.service.ts
@@ -5,17 +5,30 @@ import { Repository } from 'typeorm';
 import { CreateContactDto } from './dto/create-contact.dto';
 import { Contact } from './entities/contact.entity';
 import { PaginationDto } from '@/common/dto/pagination.dto';
+import { EmailService } from '@/shared/services/email.service';
+import { MailOptions } from '@/common/interfaces/mail-options';
 
 @Injectable()
 export class ContactService {
   constructor(
     @InjectRepository(Contact)
     private readonly contactRepository: Repository<Contact>,
+    private readonly emailService: EmailService,
   ) {}
 
   async create(createContactDto: CreateContactDto) {
     try {
       const contact = await this.contactRepository.create(createContactDto);
+
+      const options: MailOptions = {
+        to: 'officialhorlakz@gmail.com',
+        subject: 'You have a new message from form',
+        template: 'contact-message',
+        context: { name: 'There', createContactDto },
+      };
+
+      await this.emailService.sendEmail(options);
+
       return await this.contactRepository.save(contact);
     } catch (error) {
       throw new BadRequestException(error.message);
